dc935dfed3d9d5c38fc01d150a6e2581644bc3e4
ZeeshanKhan-07 <khanzeeshan4876@gmail.com>
2026-03-16 14:06:38 +0530

    Implemented Swagger

--- AUTHENTICATION/src/main/java/com/auth/authentication/config/SecurityConfig.java
+++ AUTHENTICATION/src/main/java/com/auth/authentication/config/SecurityConfig.java
@@ -44,7 +44,11 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html")
+                        .permitAll()
                         .anyRequest().authenticated())
                 .exceptionHandling(ex -> ex.authenticationEntryPoint((request, response, e) -> {
 
